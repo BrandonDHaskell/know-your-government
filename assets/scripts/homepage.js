34521b4ab9dcf155e0b9205c89145b5c7528be50
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-01-13 13:27:37 -0800

    add function implement address validation

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -1,3 +1,6 @@
+var addrEL = document.getElementById('user-input');
+var searchEl = document.getElementById('user-click');
+var addrText = addrEL.value.trim();
 
 // Gets civiv representatives data from civicinfo API
 function onSearchButtonClick(addr){
@@ -30,3 +33,27 @@ function loadFamilyTreePage(paramsStr){
         location.assign('org-tree.html');
     }
 }
+
+//function to validate address and make a call to google civic information api
+function validateInputAddr() {
+    
+    var addrRegex = /[#\/\!\@\$.;:{}%^&*()+`~]/gi; // invalid characters for address
+
+    if (!addrText || addrRegex.test(addrText)) {
+      console.log(addrText);
+      addrEL.value = "";
+      addrEL.focus();
+      document.getElementById('modalText').textContent = "Please enter valid address";
+      openModal();
+      return 
+    }
+    //api call
+    onSearchButtonClick(addrText);
+  }
+
+
+function init() {
+    searchEl.addEventListener("click",validateInputAddr);
+}
+  
+init();
