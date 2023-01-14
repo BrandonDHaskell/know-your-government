7682bc7a58b305b5928a23de37d48a9c630cfabe
Big Trees <traveye@protonmail.com>
2023-01-14 12:02:01 -0800

    updating comments /yt embed

--- assets/scripts/biopage.js
+++ assets/scripts/biopage.js
@@ -1,17 +1,4 @@
-
-
-// so need to set up API call
-// triger modals 
-// identify what data is needed from call
-// thumbnail video id link
-// const videoId = 'abc123'; // Replace with actual video ID
-// const videoLink = `https://www.youtube.com/watch?v=${videoId}`;
-// const a = document.createElement('a');
-// a.href = videoLink;
-// a.textContent = 'Watch on Youtube';
-// document.body.appendChild(a);
-// Using template literals (backticks) and the ${expression} syntax. This is the most modern and recommended way to use string interpolation in JavaScript.
-
+// Api call to YT - Renders embeded YT links to the bioPage
 const API_KEY = 'AIzaSyBz6RBLJ5i8mG7CLpH6SWfYcUTiRVa7FxA'
 var query = "Katie Porter"
 const requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${API_KEY}`;
