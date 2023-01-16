7fb8825c2d92458ff6dd20823bfb56ce0fc13e58
Brandon Haskell <bdh747flyna1@hotmail.com>
2023-01-16 14:15:14 -0800

    version 0.1 of d3 chart attempted

--- assets/scripts/org-tree.js
+++ assets/scripts/org-tree.js
@@ -1,21 +1,21 @@
 
-  //function to create representative cards and display them  
-  function createReps(repObj) {
-    var card = `<div class="column rep-card"  data-name=${repObj.repName}>
-            <h1><strong>${repObj.repName}</strong><h1>
-             <p><strong>Office: </strong>${repObj.office} <p>
-             <p><strong>Role: </strong>${repObj.officeRole}</p>
-             <p><strong>Party Name: </strong>${repObj.partyName}</p>
+  // //function to create representative cards and display them  
+  // function createReps(repObj) {
+  //   var card = `<div class="column rep-card"  data-name=${repObj.repName}>
+  //           <h1><strong>${repObj.repName}</strong><h1>
+  //            <p><strong>Office: </strong>${repObj.office} <p>
+  //            <p><strong>Role: </strong>${repObj.officeRole}</p>
+  //            <p><strong>Party Name: </strong>${repObj.partyName}</p>
              
-    </div>`;
-    document.getElementById('federal').innerHTML += card;
-  }
+  //   </div>`;
+  //   document.getElementById('federal').innerHTML += card;
+  // }
 
-  function displayReps(kygDataArr) {
-    for (var i = 0 ; i< kygDataArr.length ; i++) {
-        createReps(kygDataArr[i]); 
-    }
-  }
+  // function displayReps(kygDataArr) {
+  //   for (var i = 0 ; i< kygDataArr.length ; i++) {
+  //       createReps(kygDataArr[i]); 
+  //   }
+  // }
 
 function getKygDataObjs(){
   if( localStorage.getItem("civicRepDataObj") ){
@@ -24,20 +24,18 @@ function getKygDataObjs(){
     var offices = googObj.offices;            //  Array of civic offices
     var officials = googObj.officials;        //  Array of civic officials
     var kygDataObjs = [];                     //  Array of KYG data objects
+    var addr = getNormalizedAddrStr(googObj.normalizedInput);    //  normalized address input
 
-    // console.log(googObj);
-    // console.log(divisions);
-    // console.log(offices);
-    // console.log(officials);
-
+    console.log(googObj);
     for( var i = 0; i < officials.length; ++i ){
       var kygDataObj = {};
+      
 
-      kygDataObj[""];
+      kygDataObj["normalizedAddr"] = addr;
       kygDataObj["repName"] = officials[i].name;
       kygDataObj["partyName"] = officials[i].party;
 
-      // Somethimes address is not present
+      // Sometimes address is not present
       if( (officials[i].address in officials[i]) ){
         kygDataObj["addressStreet1"] = officials[i].address[0].line1;
         kygDataObj["addressStreet2"] = officials[i].address[0].line2;
@@ -100,17 +98,76 @@ function addCardClicks(){
 
 }
 
+
+function getTreeChart(){
+  var data = getKygDataObjs();
   
+  var nestedData = d3.nest()
+    .key(d => d.normalizedAddr)
+    .key(d => d.divisionName)
+    .key(d => d.office)
+    .entries(data);
+
+  var fData = d3.hierarchy(nestedData[0], d => d.values);
+
+  console.log(fData);
+
+  var treeLayout = d3.tree().size( [900, 800] )
+  treeLayout(fData);
+
+  var parentNumber = 27;
+  var nodes = d3.select("svg g.nodes");
+
+  // Parent node formatting
+  console.log(nodes.selectAll("cirlce").data(fData.descendants().slice(0, parentNumber)));
+  nodes.selectAll("cirlce")
+    .data(fData.descendants().slice(0, parentNumber))
+    .enter()
+    .append("cirlce")
+    .attr("class", "circle")
+    .attr("transform", d => 'translate(${d.x},${d.y})')
+    .attr("r", 8);
+
+  // Children node formatting
+  nodes.selectAll("rect")
+    .data(fData.descendants().slice(parentNumber))
+    .enter()
+    .append("rect")
+    .attr("class", "rect")
+    .attr("transform", d => ('translate(${d.x},${d.y})'))
+    .attr("transform", d => ((d.data.repName + " ").length + 4 ) * 9 )
+    .attr("height", 25)
+    .attr("y", -25 / 2 );
+}
+
+// The search is robust and a full address search is not required
+// this function formats the text to display as the page header based on 
+// any part of the address being normalized
+function getNormalizedAddrStr( normalizedObj ){
+  var addrStr = "";
+
+  // Handle stree part
+  addrStr += normalizedObj.line1;
+  addrStr += (normalizedObj.line2 in normalizedObj) ? ", " + normalizedObj.line2 : "" ;  // uncertain on this scenario, but can handle it if it exists
+
+  // if street part is empty at this point, no street address was returned
+  // ignore comma separation
+  (addrStr !== "") ? addrStr += ", " + normalizedObj.city : addrStr += normalizedObj.city;
+
+  // if addrStr is still empty, then ignore comma
+  (addrStr !== "") ? addrStr += ", " + normalizedObj.state : addrStr += normalizedObj.state;
+
+  addrStr += " " + normalizedObj.zip;
+
+  return addrStr;
+}
+
 function loadReps(){
   kygDataArr = getKygDataObjs();
   console.log("obj value");
   console.log(kygDataArr);
-  displayReps(kygDataArr);
-  addCardClicks();
-}
-
-function getTreeChart(){
-
+  // displayReps(kygDataArr);
+  // addCardClicks();
 }
 
 loadReps();
